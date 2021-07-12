8b83b66a2c76cdd7a59ab5c325910ac62585a15e
Aleksandra Czajczynska <aleksandra.czajczynska@espeo.eu>
2021-07-12 14:44:50 +0200

    add babel

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ const olaRecipe = {
   refinedMoonstone: 4,
 }
 
+const olaLetsRecipe = {
+  ...olaRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+
+}
+
 console.log(olaRecipe)
+console.log(olaLetsRecipe)
