6b4da383dfb5afe4ac030cf600f8838a56768422
Aleksandra Czajczynska <czajczynska.aleksandra@gmail.com>
2021-07-21 11:18:46 +0200

    add lodash chunk with name

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,32 +3,42 @@ const workTaskList = {
   taskNumber3: 2,
   commits: 14,
   newBranch: 3,
-}
+};
 
 const homeTaskList = {
   ...workTaskList,
   workout: 1,
   dinner: 1,
-}
+};
 
+const getLodash = () =>
+  import(
+    /* webpackChunkName: "get-lodash" */
+    'lodash-es'
+  );
 
 const Recipes = () => {
-  const [recipe, setRecipe] = useState({})
+  const [recipe, setRecipe] = useState({});
+
+  const setValue = () => {
+    setRecipe(homeTaskList);
+    getLodash();
+  };
   return (
     <div>
       <h3>Current Recipe:</h3>
       <button onClick={() => setRecipe(workTaskList)}>Work Task List</button>
-      <button onClick={() => setRecipe(homeTaskList)}>Home Task List</button>
+      <button onClick={() => setValue()}>Home Task List</button>
 
       <ul>
-      {Object.keys(recipe).map((material) => (
-        <li key={material}>
-        {material}: {recipe[material]}
-        </li>
-      ))}
+        {Object.keys(recipe).map((material) => (
+          <li key={material}>
+            {material}: {recipe[material]}
+          </li>
+        ))}
       </ul>
     </div>
-    )
+  );
 };
 
 export default Recipes;
